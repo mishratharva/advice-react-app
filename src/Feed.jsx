edc246592510b8709c1282ec7747d91816d468cc
mishratharva <mishratharva@gmail.com>
2021-06-01 10:58:25 +0530

    first commit

---
+++ src/Feed.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./feed.css";
+
+export default function Feed() {
+  const [advice1, setAdvice] = useState("");
+
+  const adviceGenerator = async () => {
+    const response = await axios.get("https://api.adviceslip.com/advice");
+
+    const { advice } = response.data.slip;
+    setAdvice(advice);
+    console.log(advice);
+  };
+  useEffect(() => {}, [advice1]);
+
+  return (
+    <div className="feed">
+      <div className="card">
+        <h1 className="heading">{advice1}</h1>
+        <button className="button" onClick={adviceGenerator}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
+    </div>
+  );
+}
